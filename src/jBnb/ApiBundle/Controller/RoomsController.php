34712b16c14aeb9158796b30222396337148cca5
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-02-15 19:17:31 +0100

    :construction: trying a first method

--- src/jBnb/ApiBundle/Controller/RoomsController.php
+++ src/jBnb/ApiBundle/Controller/RoomsController.php
@@ -2,10 +2,22 @@
 
 namespace jBnb\ApiBundle\Controller;
 
+use AppBundle\Entity\Rooms;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+/**
+ * @Route("/rooms")
+ */
 class RoomsController extends Controller
 {
-
+    /**
+     * @Route("/{id}")
+     * @Method({"GET","HEAD"})
+     */
+    public function showByIdAction(Rooms $rooms)
+    {
+        return new Response(json_encode($rooms));
+    }
 }
