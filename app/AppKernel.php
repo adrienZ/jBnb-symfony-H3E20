a3c31570940aad323bc3c2bc4ee21abae665885f
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-02-15 17:29:08 +0100

    :rocket: ApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new jBnb\ApiBundle\jBnbApiBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
