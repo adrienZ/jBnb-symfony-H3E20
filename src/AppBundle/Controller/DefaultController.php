d407a42497d4abd93af443f02ea08cab53495cb5
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-02-23 12:41:11 +0100

    :construction: send data to home views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,28 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+      $em = $this->getDoctrine()->getManager();
+      $recentRooms = $em->getRepository('AppBundle:Rooms')
+      // Get the 3 last results
+      ->findBy( [], [], 3);
+
+      // get all the offers on the website
+      $roomsCount = $em
+          ->getRepository('AppBundle:Rooms')
+          ->createQueryBuilder('AppBundle:Rooms')
+          ->select('COUNT(AppBundle:Rooms)')
+          ->getQuery()
+          ->getSingleScalarResult();
+
+          
+
+      dump($recentRooms);
+      dump($roomsCount);
         // replace this example code with whatever you need
         return $this->render('default/home.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'recentRooms' => $recentRooms,
+            'roomsCount' => $roomsCount,
         ]);
     }
 }
